fc2d972e056e95cd7afafd15bded865aaf90ff31
Eligos <2284226884@qq.com>
2026-05-14 15:53:22 +0800

    修改当前显示的Image和Text

--- UIFramework/Assets/Scripts/UIComponent/IconButton.cs
+++ UIFramework/Assets/Scripts/UIComponent/IconButton.cs
@@ -4,6 +4,10 @@ using UIComponent.CustomUI;
 using UnityEngine;
 using UnityEngine.UI;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace UIComponent
 {
     public class IconButton : MonoBehaviour
@@ -60,12 +64,56 @@ namespace UIComponent
             }
         }
 
-
         private void OnDisable()
         {
             _button.onClick.RemoveListener(OnButtonClick);
         }
-
-
     }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(IconButton))]
+    public class IconButtonEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            IconButton iconButton = (IconButton)target;
+
+            GUILayout.Space(20);
+
+            if (GUILayout.Button("Apply ImageSprite and IconText"))
+            {
+                ApplySpriteAndText(iconButton);
+            }
+        }
+
+        private void ApplySpriteAndText(IconButton iconButton)
+        {
+            SerializedObject serializedObject = new SerializedObject(iconButton);
+            
+            SerializedProperty imageSpriteProp = serializedObject.FindProperty("_imageSprite");
+            SerializedProperty iconTextProp = serializedObject.FindProperty("_iconText");
+            SerializedProperty unRaycastImageProp = serializedObject.FindProperty("_unRaycastImage");
+            SerializedProperty textMeshProProp = serializedObject.FindProperty("_textMeshProUGUI");
+
+            if (unRaycastImageProp.objectReferenceValue != null)
+            {
+                var unRaycastImage = (CustomUnRaycastImage)unRaycastImageProp.objectReferenceValue;
+                unRaycastImage.sprite = (Sprite)imageSpriteProp.objectReferenceValue;
+            }
+
+            if (textMeshProProp.objectReferenceValue != null)
+            {
+                var textMeshPro = (TMPro.TextMeshProUGUI)textMeshProProp.objectReferenceValue;
+                textMeshPro.text = iconTextProp.stringValue;
+            }
+
+            EditorUtility.SetDirty(iconButton);
+            serializedObject.ApplyModifiedProperties();
+            
+            Debug.Log("Applied imageSprite and iconText to components");
+        }
+    }
+#endif
 }
